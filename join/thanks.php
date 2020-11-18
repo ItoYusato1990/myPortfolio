38ed2ce35af4bd84364ecb3b6bd569cd8081667e
yusatoito <yusatoIto1990@gmail.com>
2020-11-18 22:21:43 +0900

    ユーザー登録完了画面の作成

---
+++ join/thanks.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+
+$_SESSION['email'] = $_SESSION['join']['email'];
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <p>登録が完了しました。</p>
+  <a href="../mybooks/mybooks_list.php">トップページへ</a>
+</body>
+</html>
